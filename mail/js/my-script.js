9903f3915bc32b298e307e96bc57fd3293c5edd9
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-25 16:13:19 +0200

    Aggiunge un po' di grafica all'email

--- mail/js/my-script.js
+++ mail/js/my-script.js
@@ -11,14 +11,32 @@ stampa un messaggio appropriato sull’esito del controllo.
 const emails = ['raimondo@gmail.com', 'roberto@gmail.com', 'alfredo@gmail.com', 'giulio@gmail.com'];
 
 // 2. hiedo all'utente di inserire la sua email
-const userEmail = prompt('Inserisci la tua email:')
+// const userEmail = prompt('Inserisci la tua email:')
 
-// 3. Pongo le condizioni di verifica
-for (let i = 0; i < emails.length; i++) {
-    if ( userEmail === emails[i]) {
-        alert(`L'email è valida`);
-    } else {
-        console.log(`L'email non è presente nel nostro database`);
+const triggerBtn = document.querySelector('#btn-trigger');
+const warning = document.querySelector('.warning-email');
+
+triggerBtn.addEventListener('click', function() {
+    const userEmail = document.getElementById('user-email').value;
+    const inputEmail = document.getElementById('input-email');
+    const warning = document.querySelector('.warning-email');
+    warning.innerHTML = `<span style="color: red;">L'email non è presente nel nostro database</span>`;
+    for (let i = 0; i < emails.length; i++) {
+        if ( userEmail === emails[i]) {
+            warning.innerHTML = `<span style="color: green;">L'email è valida</span>`;
+            warning.classList.add('show');
+        } else {
+            warning.classList.add('show');
+            inputEmail.classList.add('shake');
+        }
     }
-    console.log(userEmail, emails[i]);
-}
+});
+// 3. Pongo le condizioni di verifica
+// for (let i = 0; i < emails.length; i++) {
+//     if ( userEmail === emails[i]) {
+//         alert(`L'email è valida`);
+//     } else {
+//         console.log(`L'email non è presente nel nostro database`);
+//     }
+//     console.log(userEmail, emails[i]);
+// }
