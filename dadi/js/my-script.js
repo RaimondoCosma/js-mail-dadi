b8d950a210e4bdbd0cc201e387664718dcd82659
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-25 14:17:58 +0200

    Dichiaro le variabili per generare i numeri

--- dadi/js/my-script.js
+++ dadi/js/my-script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+/*
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+// 1. Dichiaro la variabile che genera un numero random da 1 a 6 per l'utente
+let userNumber = Math.floor(Math.random() * 6) + 1;
+// 1. Dichiaro la variabile che genera un numero random da 1 a 6 per il bot
+let botNumber = Math.floor(Math.random() * 6) + 1;
+console.log(userNumber, botNumber)
