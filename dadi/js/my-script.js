7c2757283b56426d6df76498768ac943b81d692f
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-25 14:24:14 +0200

    Pone condizioni per lavittoria

--- dadi/js/my-script.js
+++ dadi/js/my-script.js
@@ -8,6 +8,16 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 // 1. Dichiaro la variabile che genera un numero random da 1 a 6 per l'utente
 let userNumber = Math.floor(Math.random() * 6) + 1;
-// 1. Dichiaro la variabile che genera un numero random da 1 a 6 per il bot
+// 2. Dichiaro la variabile che genera un numero random da 1 a 6 per il bot
 let botNumber = Math.floor(Math.random() * 6) + 1;
 console.log(userNumber, botNumber)
+
+// 3. Pongo le condizione per la vittoria
+if (userNumber > botNumber) {
+    alert(`Complimenti! Hai vinto. Ricarica la pagina per continuare a giocare`);
+} else if (userNumber === botNumber) {
+    alert(`Pareggio`);
+    window.location.reload();
+} else if (userNumber < botNumber) {
+    alert(`Mi dispiace! Hai perso. Ricarica la pagina per continuare a giocare`);
+}
