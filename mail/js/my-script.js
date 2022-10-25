df85e04fc0cc53505a2e9a787fa685c2f6f7c531
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-25 13:29:21 +0200

    Aggiunge array e prompt

--- mail/js/my-script.js
+++ mail/js/my-script.js
@@ -1,1 +1,14 @@
 'use stricy';
+
+/*
+Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+*/
+
+// 1. Creo una lista contenente delle email 
+const emails = ['raimondo@gmail.com', 'roberto@gmail.com', 'alfredo@gmail.com', 'giulio@gmail.com'];
+
+// 2. hiedo all'utente di inserire la sua email
+const userEmail = prompt('Inserisci la tua email:')
